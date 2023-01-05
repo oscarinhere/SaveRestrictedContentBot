fc3e2b865ee5d4dd07d7317e206b9b4c37673ad3
oscarinhere <102413753+oscarinhere@users.noreply.github.com>
2023-01-05 08:05:12 +0700

    Update __init__.py
    Signed-off-by: oscarinhere <102413753+oscarinhere@users.noreply.github.com>

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("21332328", default=None, cast=int)
-API_HASH = config("a2215944df845b4f64b4e8b7c71c84ac", default=None)
-BOT_TOKEN = config("5983735642:AAGFowFBm435ql_hIT9dodMgHUliMfh1TCo", default=None)
-SESSION = config("BQA9Y_REpkS_FsgjEfTb4lR48IOYyyFG7WQSvV65mpZrbYL_LUBXxItE-SZ9Itw-ToFHiYDO8kpUommdWUsUw_-DCt3kaZInxAc0uRhjsvrAr0dT5lf0uG1gRsDK6YQL-6DAm5WK3klTh3P8GyP-tohk-JxD3-Cr3UK2JgJqn1jVH_7W8oYuOhIwNicxRTRDKe2dSXY79WpQy-oaTuYW7kE3KlFzO4c6QnqaujFewSd0L8F_tHQWXJQph4LoHGY0tlvBptYFT9U7AETzgvej5KE6HFVjePqCdKpvQerXZiMYg3T_PoC3sILUcPG2oOYnIsOj9IXvgftSwqHKQPTs0blJdgQptQA", default=None)
-FORCESUB = config("genkpetirinfo", default=None)
-AUTH = config("2062576370", default=None, cast=int)
+API_ID = "21332328"
+API_HASH = "a2215944df845b4f64b4e8b7c71c84ac"
+BOT_TOKEN = "5983735642:AAGFowFBm435ql_hIT9dodMgHUliMfh1TCo"
+SESSION = "BQA9Y_REpkS_FsgjEfTb4lR48IOYyyFG7WQSvV65mpZrbYL_LUBXxItE-SZ9Itw-ToFHiYDO8kpUommdWUsUw_-DCt3kaZInxAc0uRhjsvrAr0dT5lf0uG1gRsDK6YQL-6DAm5WK3klTh3P8GyP-tohk-JxD3-Cr3UK2JgJqn1jVH_7W8oYuOhIwNicxRTRDKe2dSXY79WpQy-oaTuYW7kE3KlFzO4c6QnqaujFewSd0L8F_tHQWXJQph4LoHGY0tlvBptYFT9U7AETzgvej5KE6HFVjePqCdKpvQerXZiMYg3T_PoC3sILUcPG2oOYnIsOj9IXvgftSwqHKQPTs0blJdgQptQA")
+FORCESUB = "genkpetirinfo"
+AUTH = "2062576370"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
